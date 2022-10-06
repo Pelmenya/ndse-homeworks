0bc542695b08072dc7ae7b2c44ea6a12c45c1695
Dmitry Lyapin <lyapindm@yandex.ru>
2022-10-06 14:43:35 +0300

    Сделал задание 1.5

--- src/homework-1-5/get-weather-in-town.ts
+++ src/homework-1-5/get-weather-in-town.ts
@@ -6,24 +6,43 @@ import http from 'http';
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
+export interface IWeather {
+    current: {
+        temperature: number;
+    },
+    location: {
+        name: string;
+        temperature: number;
+    }
+    forecast: {
+        [key: string]: {
+            date: string;
+            mintemp: number;
+            maxtemp: number;
+            avgtemp: number;
+        }
+    }
+}
+
 const { accessKey } = process.env;
 const SERVER_URL = 'http://api.weatherstack.com';
 
 const main = (key: string) => {
+    const controlKeys = ['q', 'Q'];
+
     const rl = readline.createInterface({ input, output });
 
     console.log('Введите город, в котором хотите узнать погоду. Выйти - q.');
 
     const handlerLine = (i: string) => {
-        console.log(i);
         if (i === 'q' || i === 'Q') {
             console.log('Удачи!!!');
             rl.close();
             rl.off('line', handlerLine);
             input.unref();
         }
-        if (i) {
-            const url = `${SERVER_URL}/current?access_key=${key}&query=${i}`;
+        if (i && !controlKeys.includes(i)) {
+            const url = `${SERVER_URL}/forecast?access_key=${key}&query=${i}`;
             http.get(url, (res) => {
                 const { statusCode } = res;
                 if (statusCode) {
@@ -34,8 +53,16 @@ const main = (key: string) => {
                             rowData += chunk;
                         });
                         res.on('end', () => {
-                            const parseData = JSON.parse(rowData);
-                            console.log(parseData);
+                            const parseData: IWeather = JSON.parse(rowData);
+                            if (parseData.location) {
+                                const { location, current, forecast } = parseData;
+                                const day = forecast[Object.keys(forecast)[0]];
+                                console.log(`Сейчас в городе ${location.name}`, current.temperature, '°C');
+                                console.log(`Прогноз погоды от ${day.date}:`);
+                                console.log('Минимальная температура: ', day.mintemp, '°C');
+                                console.log('Максимальная температура: ', day.maxtemp, '°C');
+                                console.log('Средняя температура: ', day.avgtemp, '°C');
+                            } else console.log(`Город «${i}» не найден`);
                         });
                     } else console.log(`statusCode: ${statusCode}`);
                 } else console.log('statusCode: undefined');
